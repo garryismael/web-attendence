c70b935f1a4ae6efeb78521663faf2f70cf19f3d
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 22:58:22 +0300

    add button for upload file

--- src/app/pages/attendance/attendance.component.ts
+++ src/app/pages/attendance/attendance.component.ts
@@ -21,17 +21,15 @@ import { ReactiveFormsModule } from '@angular/forms';
 })
 export class AttendanceComponent {
   attendances = attendances;
+  file: File | null = null;
 
   constructor(private msg: NzMessageService) {}
 
-  handleChange(info: NzUploadChangeParam): void {
-    if (info.file.status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (info.file.status === 'done') {
-      this.msg.success(`${info.file.name} file uploaded successfully`);
-    } else if (info.file.status === 'error') {
-      this.msg.error(`${info.file.name} file upload failed.`);
+  handleChange(event: any): void {
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.file = file;
     }
   }
 }
