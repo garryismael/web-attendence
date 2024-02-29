353dc756e515244b78e9c14c054db15bddfe0fef
Garry <tahinjanaharygarry@gmail.com>
2024-02-29 19:34:41 +0300

    setup routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { AttendanceComponent } from './pages/attendance/attendance.component';
+import { EmployeeComponent } from './pages/employee/employee.component';
+import { LayoutComponent } from './pages/layout/layout.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'attendances',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'employee',
+        component: EmployeeComponent,
+      },
+      {
+        path: 'attendances',
+        component: AttendanceComponent,
+      },
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
