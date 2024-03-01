2888d39ac87e8ffd9455743997f2d261faa1aa8e
Garry <tahinjanaharygarry@gmail.com>
2024-03-01 20:23:07 +0300

    display attendance data

--- src/app/pages/attendance/attendance.component.ts
+++ src/app/pages/attendance/attendance.component.ts
@@ -1,30 +1,29 @@
-import { Component, afterNextRender, inject } from '@angular/core';
+import { Component, OnInit, afterNextRender, inject } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Attendance } from '../../models';
 import { readExcel } from '../../utils';
 import { AttendanceService } from './attendance.service';
+import { DateFormat } from '../../utils/date';
 @Component({
   selector: 'app-attendance',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DateFormat],
   templateUrl: './attendance.component.html',
   styleUrl: './attendance.component.css',
 })
-export class AttendanceComponent {
+export class AttendanceComponent implements OnInit {
   attendances: Attendance[] = [];
   file: File | null = null;
 
   private attendanceService = inject(AttendanceService);
 
-  constructor() {
-    afterNextRender(() => {
-      this.findAll();
-    });
+  ngOnInit(): void {
+    this.findAll();
   }
 
   findAll() {
     this.attendanceService.findAll().subscribe((attendances) => {
-      attendances = attendances;
+      this.attendances = attendances;
     });
   }
 
