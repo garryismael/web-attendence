dd3f1aed4632def3b44f6de97360c68d2caca8f7
Garry <tahinjanaharygarry@gmail.com>
2024-03-02 07:53:05 +0300

    use env prod

--- src/app/utils/http.ts
+++ src/app/utils/http.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
